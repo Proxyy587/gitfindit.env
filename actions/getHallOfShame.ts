d78033bf6f39c03a14f626112f6d7fac4cf515d2
Abhijit <abhijitbhattacharjee333@gmail.com>
2024-11-02 17:27:20 +0530

    error thing was personal lmao

--- actions/getHallOfShame.ts
+++ actions/getHallOfShame.ts
@@ -1,9 +1,14 @@
 "use server";
 
 import { User } from "@/lib/user.model";
+import { connectToDatabase } from "@/lib/db";
 
 export const getHallOfShame = async () => {
-	const users = await User.find().sort({ leakedCount: -1 });
+	await connectToDatabase();
+	const users = await User.find()
+		.sort({ leakedCount: -1 })
+		.limit(10)
+		.select("username leakedCount lastChecked");
     
 	return users;
 };
