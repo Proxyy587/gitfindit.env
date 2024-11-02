ca22b6e76f304c4ed231e5ecdbaeae0aba968680
Abhijit <abhijitbhattacharjee333@gmail.com>
2024-11-02 17:26:11 +0530

    added dynamic to theh recentview api

--- app/api/recent-views/route.ts
+++ app/api/recent-views/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { User } from "@/lib/user.model";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
 	try {
 		const recentViews = await User.find()
